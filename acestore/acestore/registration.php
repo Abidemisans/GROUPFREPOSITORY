ecbba265e691077090684a96552ace878ba6b7f1
Abidemisans <sanabidemi@gmail.com>
2023-03-22 19:57:05 +0000

    File management

---
+++ acestore/acestore/registration.php
@@ -0,0 +1,23 @@
+<?php
+    require_once('connection.php');
+    // When form submitted, insert values into the database.
+    if (isset($_REQUEST['username'])) {
+        // removes backslashes
+        $username = stripslashes($_REQUEST['username']);
+        //escapes special characters in a string
+        $username = mysqli_real_escape_string($con, $username);
+        $email    = stripslashes($_REQUEST['email']);
+        $email    = mysqli_real_escape_string($con, $email);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        $query    = "INSERT into `users` (username, password, email)
+                   VALUES ('$username', '" . md5($password) . "', '$email')";
+        $result   = mysqli_query($con, $query);
+        if ($result) {
+            header("Location: index.html");
+        } else {
+            echo "Required fields are missing";
+        }
+    } else {
+    }
+?>
